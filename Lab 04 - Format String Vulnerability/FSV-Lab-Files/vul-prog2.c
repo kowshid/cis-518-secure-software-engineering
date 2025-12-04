d4983ba5c805d9f4e12a7dad4fe37e438b59b277
Ahsanul Kabir <kowshid@gmail.com>
2025-12-04 15:17:20 -0500

    SEED Lab 4: Format String Vulnerability

---
+++ Lab 04 - Format String Vulnerability/FSV-Lab-Files/vul-prog2.c
@@ -0,0 +1,39 @@
+/* vul-prog2.c */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SECRET1 0x44
+#define SECRET2 0x55
+
+int main(int argc, char *argv[])
+{
+    char user_input[100];
+    int *secret;
+    int a, b, c, d; /* other variables, not used. */
+    
+    /* The secret value is stored on the heap */
+    secret = (int *) malloc(2 * sizeof(int));
+    
+    /* getting the secret */
+    secret[0] = SECRET1;
+    secret[1] = SECRET2;
+    
+    printf("The variable secret's address is 0x%08x (on stack)\n", (unsigned int) &secret);
+    printf("The variable secret's value is 0x%08x (on heap)\n", (unsigned int) secret);
+    printf("secret[0]'s address is 0x%08x (on heap)\n", (unsigned int) &secret[0]);
+    printf("secret[1]'s address is 0x%08x (on heap)\n", (unsigned int) &secret[1]);
+    
+    printf("Enter a string:\n");
+    scanf("%s", user_input); /* getting a string from user */
+    
+    /* Vulnerable place */
+    printf(user_input);
+    printf("\n");
+    
+    /* Verify whether your attack is successful */
+    printf("The original secrets: 0x%x -- 0x%x\n", SECRET1, SECRET2);
+    printf("The new secrets:      0x%x -- 0x%x\n", secret[0], secret[1]);
+
+    return 0;
+}
